a6a01390d2e221ed14e50cab9ca37cfd17c3735d
Mario Villasante <mario.villasante@islas.org.mx>
2023-08-25 14:36:13 -0700

    ✨📈 run first example

---
+++ src/streamlit_app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# GECI
+Hello world
+V3
+""")
+df = pd.read_csv("tests/data/estaciones_guadalupe.csv")
+st.line_chart(df, x="Time", y = "Temp_Out")
+
